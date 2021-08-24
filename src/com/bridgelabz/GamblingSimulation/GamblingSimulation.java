7cdf85af00b1129f56c3fed5de982f9315710d21
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 22:34:46 +0530

    ADDED : Total amout for 20 days

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -4,38 +4,47 @@ public class GamblingSimulation
 {
 	public static final int STARTING_STAKE = 100;
 	public static final int EVERY_GAME_BET = 1;
+	public static final int DAYS_PLAYED = 20;
 	public static int currentStake;
-	
 	
 	public static void makeBet() {
 		
 		if(Math.random() > 0.5) {
 			currentStake+=EVERY_GAME_BET;
-			System.out.println("Won $"+EVERY_GAME_BET);
 		}
 		else {
 			currentStake-=EVERY_GAME_BET;
-			System.out.println("Lost $"+EVERY_GAME_BET);
 		}
 		
-	}
-	public static Boolean checkResignGame() {
-		
-		if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
-			return true;
-		}
-		else
-			return false;
 	}
 	
-	public static void main(String[] args) {
-		currentStake = STARTING_STAKE;
+	public static void checkResignGame() {
 		Boolean shouldResign=false;
 		while(!shouldResign) {
 			makeBet();
-			shouldResign=checkResignGame();
+			if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
+				shouldResign= true;
+			}
+			else
+				shouldResign= false;
 		}
-		System.out.println("Resigning game with $"+currentStake);
+		
+	}
+	
+	public static void playFor20Days() {
+		int totalWinnings = 0;
+		for (int i = 1; i <= DAYS_PLAYED; i++) {
+			currentStake = STARTING_STAKE;
+			checkResignGame();
+			totalWinnings+=currentStake;
+			System.out.println("Resigning Day "+i+" with $"+currentStake);
+		}
+		System.out.println("Total winnings after "+DAYS_PLAYED+" days: "+totalWinnings);
+	}
+	public static void main(String[] args) {
+		
+		playFor20Days();
+		
 	}
 
 }
