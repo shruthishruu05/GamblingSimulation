2d131d6697e91e5ab6b6e9cdc9b59516cca55880
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 23:16:13 +0530

    Modified : total amount after 20 days

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class GamblingSimulation {
 	public static final int STARTING_STAKE = 100;
 	public static final int GAME_BET = 1;
+	public static final int DAYS_PLAYED = 30;
 	public static int currentStake;
 	
 	
@@ -25,6 +26,16 @@ public class GamblingSimulation {
 		}
 		else
 			return false;
+	}
+	public static void playFor20Days() {
+		int totalWinning = 0;
+		for (int i = 1; i <= DAYS_PLAYED; i++) {
+			currentStake = STARTING_STAKE;
+			checkResign();
+			totalWinning=totalWinning+currentStake;
+			System.out.println("Resigning Day "+i+" with $"+currentStake);
+		}
+		System.out.println("Total win after "+DAYS_PLAYED+" days: "+totalWinning);
 	}
 	
 	public static void main(String[] args) {
