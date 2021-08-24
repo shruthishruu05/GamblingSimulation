1ab809229503600a82a71dad0afdea4689e3d5b5
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 23:12:02 +0530

    Modified : Player plays till he wins or loses 50%

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -1,53 +1,44 @@
 package com.bridgelabz.GamblingSimulation;
 import java.util.*;
-public class GamblingSimulation 
-{
+public class GamblingSimulation {
 	public static final int STARTING_STAKE = 100;
-	public static final int EVERY_GAME_BET = 1;
-	public static final int DAYS_PLAYED = 20;
+	public static final int GAME_BET = 1;
 	public static int currentStake;
 	
-	public static void makeBet() {
+	
+	public static void Bet() {
 		
 		if(Math.random() > 0.5) {
-			currentStake+=EVERY_GAME_BET;
+			currentStake=GAME_BET;
+			System.out.println("Won $"+GAME_BET);
 		}
 		else {
-			currentStake-=EVERY_GAME_BET;
+			currentStake-=GAME_BET;
+			System.out.println("Lost $"+GAME_BET);
 		}
 		
 	}
-	
-	public static void checkResignGame() {
-		Boolean shouldResign=false;
-		while(!shouldResign) {
-			makeBet();
-			if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
-				shouldResign= true;
-			}
-			else
-				shouldResign= false;
-		}
+	public static Boolean checkResign() {
 		
+		if(currentStake >= 1.5*STARTING_STAKE || currentStake <=0.5* STARTING_STAKE) {
+			return true;
+		}
+		else
+			return false;
 	}
 	
-	public static void playFor20Days() {
-		int totalWinnings = 0;
-		for (int i = 1; i <= DAYS_PLAYED; i++) {
-			currentStake = STARTING_STAKE;
-			checkResignGame();
-			totalWinnings+=currentStake;
-			System.out.println("Resigning Day "+i+" with $"+currentStake);
-		}
-		System.out.println("Total winnings after "+DAYS_PLAYED+" days: "+totalWinnings);
-	}
 	public static void main(String[] args) {
-		
-		playFor20Days();
-		
+		currentStake = STARTING_STAKE;
+		Boolean Resign=false;
+		while(!Resign) {
+			Bet();
+			Resign=checkResign();
+		}
+		System.out.println("Resign game with the stake as: $"+currentStake);
 	}
 
 }
+
 
 
 
