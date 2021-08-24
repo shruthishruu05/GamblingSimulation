70f0681a428aaf3595d812cde46fac4157d04442
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 22:30:21 +0530

    ADDED : User resigning

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -4,23 +4,41 @@ public class GamblingSimulation
 {
 	public static final int STARTING_STAKE = 100;
 	public static final int EVERY_GAME_BET = 1;
+	public static int currentStake;
+	
+	
 	public static void makeBet() {
 		
 		if(Math.random() > 0.5) {
+			currentStake+=EVERY_GAME_BET;
 			System.out.println("Won $"+EVERY_GAME_BET);
 		}
 		else {
+			currentStake-=EVERY_GAME_BET;
 			System.out.println("Lost $"+EVERY_GAME_BET);
 		}
 		
 	}
+	public static Boolean checkResignGame() {
+		
+		if(currentStake >= STARTING_STAKE*1.5 || currentStake <= STARTING_STAKE*0.5) {
+			return true;
+		}
+		else
+			return false;
+	}
 	
 	public static void main(String[] args) {
-		
-		makeBet();
-		
+		currentStake = STARTING_STAKE;
+		Boolean shouldResign=false;
+		while(!shouldResign) {
+			makeBet();
+			shouldResign=checkResignGame();
+		}
+		System.out.println("Resigning game with $"+currentStake);
 	}
 
 }
+
 
 
