fc20ac9b9ce5d71f503ea0af458f02e63e040f2c
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 22:06:41 +0530

    ADDED : Intital stake and bet money

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -1,20 +1,10 @@
 package com.bridgelabz.GamblingSimulation;
 import java.util.*;
 public class GamblingSimulation {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("enter the stake value");
-		int stake = in.nextInt();
-		System.out.println("enter the bet value");
-		int bet = in.nextInt();
-		if(stake ==100 ||bet ==1)
-		{
-			System.out.println("The player can start the game");
-		}
-		else
-		{
-			System.out.println("the player cannot start the game");
-		}
+public static void main(String[] args) {
+		
+		int STARTING_STAKE = 100;
+		int EVERY_GAME_BET = 1;
 		
 	}
 }
