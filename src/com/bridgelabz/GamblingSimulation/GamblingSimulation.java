8c3606c092255dbd707496fcef68de95c6b6ef9f
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 22:25:40 +0530

    ADDED : Player placing a bet

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -1,13 +1,26 @@
 package com.bridgelabz.GamblingSimulation;
 import java.util.*;
-public class GamblingSimulation {
-public static void main(String[] args) {
+public class GamblingSimulation 
+{
+	public static final int STARTING_STAKE = 100;
+	public static final int EVERY_GAME_BET = 1;
+	public static void makeBet() {
 		
-		int STARTING_STAKE = 100;
-		int EVERY_GAME_BET = 1;
-		System.out.println("stake: "+STARTING_STAKE);
-		System.out.println("stake: "+EVERY_GAME_BET );
-		
+		if(Math.random() > 0.5) {
+			System.out.println("Won $"+EVERY_GAME_BET);
+		}
+		else {
+			System.out.println("Lost $"+EVERY_GAME_BET);
+		}
 		
 	}
+	
+	public static void main(String[] args) {
+		
+		makeBet();
+		
+	}
+
 }
+
+
