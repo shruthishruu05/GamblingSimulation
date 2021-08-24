609fb4b7a536d86bb922bed3dedf972d36cff3ce
shruthishruu05 <shruthi.m@perfios.com>
2021-08-24 22:19:44 +0530

    ADDED: Initial stake and bet

--- src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation/GamblingSimulation.java
@@ -5,6 +5,9 @@ public static void main(String[] args) {
 		
 		int STARTING_STAKE = 100;
 		int EVERY_GAME_BET = 1;
+		System.out.println("stake: "+STARTING_STAKE);
+		System.out.println("stake: "+EVERY_GAME_BET );
+		
 		
 	}
 }
